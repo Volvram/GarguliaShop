718d87cd2b4248d033fdd6ca055eaae412e7c2ec
Julia <yuliabobrovich@yandex.ru>
2023-01-18 03:03:08 +0300

    Prices are up

--- src/config/services.ts
+++ src/config/services.ts
@@ -11,7 +11,7 @@ export const SERVICES: ServicesType[] = [
     id: "saveAllLoot",
     title: "Сопровождение",
     description: "Включает в себя файты и радку, плюс сейв всего лута с типов",
-    price: 125,
+    price: 150,
     currency: "rub",
   },
   {
@@ -19,21 +19,21 @@ export const SERVICES: ServicesType[] = [
     title: "Сопровождение",
     description:
       "Включает в себя файты и радку, что сможешь взять, то и унесёшь",
-    price: 90,
+    price: 115,
     currency: "rub",
   },
   {
     id: "fights",
     title: "Сопровождение",
     description: "Включает в себя файты",
-    price: 80,
+    price: 105,
     currency: "rub",
   },
   {
     id: "rad",
     title: "Сопровождение",
     description: "Включает в себя радку",
-    price: 80,
+    price: 105,
     currency: "rub",
   },
 ];
